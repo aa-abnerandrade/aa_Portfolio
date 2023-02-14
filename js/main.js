20925a0fc63948d107f203bf3f651edf1934bfc2
Abner Andrade <aa.abnerandrade@gmail.com>
2023-02-14 17:45:25 -0300

    Conclusão de página inicial e criação de arquivos de dados

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 
 const allMainPage = document.querySelector('.cont-main');
-let showBody = true;
+const acordeonTriggers = document.querySelectorAll('.card__trigger');
 
+acordeonTriggers.forEach((trigger)=> {
+    trigger.addEventListener('click', (e)=> {
+        const acordeon = trigger.parentElement;
+        const isOpen = acordeon.classList.contains('showAcordeon');
 
+        if (isOpen) {
+            acordeon.classList.remove('showAcordeon');
+        } else {
+            acordeon.classList.add('showAcordeon');
+        }
+
+    })
+})
