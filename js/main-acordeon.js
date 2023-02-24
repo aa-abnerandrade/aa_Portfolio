c2e9c5c38f815671cda5b0d1a69d0f19f9e71059
Abner Andrade <aa.abnerandrade@gmail.com>
2023-02-23 22:54:53 -0300

    Ajustes na seção Projetos da Index.

--- js/main-acordeon.js
+++ js/main-acordeon.js
@@ -1,19 +1,31 @@
 
-const allMainPage = document.querySelector('.cont-main');
 const acordeonTriggers = document.querySelectorAll('.card__trigger');
 
+
 acordeonTriggers.forEach((trigger)=> {
+    console.log(acordeonTriggers);
+    
     trigger.addEventListener('click', (e)=> {
         const acordeon = trigger.parentElement;
         const isOpen = acordeon.classList.contains('showAcordeon');
 
+        let allOpen = document.querySelectorAll('.showAcordeon');
+        console.log(allOpen);
+
+        
+
         if (isOpen) {
             acordeon.classList.remove('showAcordeon');
         } else {
+            allOpen.forEach((openedAcordeon)=> {
+                openedAcordeon.classList.remove('showAcordeon');
+            })
             acordeon.classList.add('showAcordeon');
         }
 
     })
+
+    
 })
 
 
