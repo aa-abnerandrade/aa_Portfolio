c2a87008ecaaefd958e64b8e47f5f4690297430d
Abner Andrade <aa.abnerandrade@gmail.com>
2023-03-07 20:28:52 -0300

    Completando Infos no JSON

--- js/about.js
+++ js/about.js
@@ -1,5 +1,7 @@
 const urlProfile = 'https://raw.githubusercontent.com/aa-abnerandrade/meu_portfolio/improvement-responsividadetotal/data/profile.json';
 
+const urlLinksContact = 'https://raw.githubusercontent.com/aa-abnerandrade/meu_portfolio/improvement-responsividadetotal/data/contacts.json';
+
 (async ()=> {
     const jProfileData = await getFileData(urlProfile);
     console.log(jProfileData);
