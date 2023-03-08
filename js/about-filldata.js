b5f0e6620127469046b7ab6a76737e0d1a934fa6
Abner Andrade <aa.abnerandrade@gmail.com>
2023-03-07 21:33:22 -0300

    Finalização para Deploy

--- js/about-filldata.js
+++ js/about-filldata.js
@@ -3,29 +3,29 @@ const lstLinksSocialHome = document.querySelector('.sec-overview__social--list')
 const lstLinksSocialFooter = document.querySelector('.cont-footer__social__list');
 
 function showLinksSocialInAboutHTML(jsonLinksSocial) {
-    const objLinkSoci = jsonLinksSocial.links;
 
-    objLinkSoci.forEach((contact)=> {
-
-        lstLinksSocialHome.innerHTML +=
+    lstLinksSocialHome.innerHTML +=
+    jsonLinksSocial.links.map(contact=> 
         `
             <li>
                 <a href="${contact.url}" target="_blank">
                     <i class="uil ${contact.icon}"></i>
                 </a>
             </li>
-        
-        `;
+        `
+    ).join('');
 
-        lstLinksSocialFooter.innerHTML += 
+
+    lstLinksSocialFooter.innerHTML +=
+    jsonLinksSocial.links.map(contact=>
         `
             <li>
                 <a href="${contact.url}" target="_blank">
                     <i class="uil ${contact.icon}"></i>
                 </a>
             </li>
-        `;
-    })
+        `         
+    ).join('');
 
 }
 
